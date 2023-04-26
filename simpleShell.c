dfa54b75f3bfff7635df012c45b93aeceae90184
Mahmoud Helmy <mahmoud98398@gmail.com>
2023-04-26 16:48:29 -0700

    Add Rest Files

--- simpleShell.c
+++ simpleShell.c
@@ -1,110 +1,51 @@
 #include "helpers.h"
-
 /**
- * print_prompt - prints a prompt
- * Return: void
+ * main - Main arguments functions
+ * @ac: Count of argumnents
+ * @av: Arguments
+ * @env: Environment
+ * Return: _exit = 0.
  */
-
-void print_prompt(void)
+int main(int ac, char **av, char **env)
 {
-	struct utsname buf;
-	char buf2[200];
-
-	uname(&buf);
-	getcwd(buf2, 200);
-	printf("%s@%s:%s$ ", getenv("USER"), buf.nodename, buf2);
-}
-
-/**
- * get_line - get line
- * @line: input line
- * Return: result line
- */
-char *get_line(char *line)
-{
-	fgets(line, 100, stdin);
-	line[strcspn(line, "\n")] = 0;
-	return (line);
-}
-
-/**
- * parser - parsing input to choices
- * @_cmd: first command ketword
- * @cmd: entire command line
- * @params: parameters
- * Return: choice
- */
-int parser(char *_cmd, char *cmd, char **params)
-{
-	char *temp[50];
-	int i = 0;
-	int j;
-	char *token = strtok(_cmd, " ");
-
-	while (token != NULL)
+	int pathValue = 0, status = 0, is_path = 0;
+	char *line = NULL, /**ptr to inpt*/ **commands = NULL; /**tokenized commands*/
+	(void)ac;
+	while (1)/* loop until exit */
 	{
-		temp[i++] = token;
-		token = strtok(NULL, " ");
-	}
-	if (strcmp(_cmd, "exit") == 0)
-		return (-EXIT);
-	if (strcmp(_cmd, "^D") == 0)
-		return (END_OF_FILE);
-	if (strcmp(_cmd, "env") == 0)
-		return (PRINT_ENV);
-	strcpy(cmd, temp[0]);
-	for (j = 0; j < i; j++)
-		params[j] = temp[j];
-	params[i] = NULL;
-	return (RET);
-}
-
-/**
- * main - main function
- * Return: int
- */
-
-int main(void)
-{
-	int status, ch, ret_pid;
-	char myline[100], cmd[50], check_command[100];
-	char *params[50] = {NULL}, **env = NULL;
-
-	while (1)
-	{
-		print_prompt();
-		get_line(myline);
-		if (strcmp(myline, "\0") == 0)
-			continue;
-		ch = parser(myline, cmd, params);
-		if (ch == -EXIT)
-			exit(0);
-		if (ch == END_OF_FILE)
+		errno = 0;
+		line = _getline_command();/** reads user input*/
+		if (line == NULL && errno == 0)
+			return (0);
+		if (line)
 		{
-			end_file(myline);
-			break;
-		}
-		if (ch == PRINT_ENV)
-			print_enviro(env);
-		if (ch == RET)
-		{
-			sprintf(check_command, "which %s >/dev/null 2>&1", params[0]);
-			if (system(check_command) == 0)
-			{
-				ret_pid = fork();
-				if (ret_pid < 0)
-					printf("Fork Faild \n");
-				else if (ret_pid > 0)
-					wait(&status);
-				else
-				{
-					execvp(cmd, params);
-					printf("%s Command Not Found\n", params[0]);
-				}
-			}
+			pathValue++;
+			commands = tokenize(line);/** tokenizes or parse user input*/
+			if (!commands)
+				free(line);
+			if (!_strcmp(commands[0], "env"))/**checks if user wrote env"*/
+				_getenv(env);
 			else
-			printf("%s Command Not Found\n", params[0]); 
+			{
+				is_path = _values_path(&commands[0], env);/** tokenizes PATH*/
+				status = _fork_fun(commands, av, env, line, pathValue, is_path);
+					if (status == 200)
+					{
+						free(line);
+						return (0);
+					}
+				if (is_path == 0)
+					free(commands[0]);
+			}
+			free(commands); /*free up memory*/
 		}
-	} return (RET);
+		else
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);/** Writes to standard output*/
+			exit(status);
+		}
+		free(line);
+	}
+	return (status);
 }
-
